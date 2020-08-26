c4ba7987fb59c1fb9ba6c65983d3275eb13f23a1
Patryk <patryk.lekki1@gmail.com>
2020-08-26 23:59:50 +0200

    add autoprefixer + css compress

--- script/app.js
+++ script/app.js
@@ -1,137 +1,132 @@
-const hamburger = document.querySelector(".hamburger");
-const headerList = document.querySelector(".header-list");
-const headerLink = document.querySelector(".header__link");
-const headerNav = document.querySelector(".header__nav");
+// const hamburger = document.querySelector(".hamburger");
+// const headerList = document.querySelector(".header-list");
+// const headerLink = document.querySelector(".header__link");
+// const headerNav = document.querySelector(".header__nav");
 
-hamburger.addEventListener("click", () => {
-  headerList.classList.toggle("links-active");
-  hamburger.classList.toggle("active-ham");
-  headerNav.classList.toggle("nav-active");
-  headerLink.classList.toggle("blue-link");
-});
+// hamburger.addEventListener("click", () => {
+//   headerList.classList.toggle("links-active");
+//   hamburger.classList.toggle("active-ham");
+//   headerNav.classList.toggle("nav-active");
+//   headerLink.classList.toggle("blue-link");
+// });
 
 // First animation
 let tlFirst = gsap.timeline({
-  scrollTrigger:{
-    trigger:".healthcare",
-    start:"center bottom "
-  }
-})
+  scrollTrigger: {
+    trigger: ".healthcare",
+    start: "center bottom ",
+  },
+});
 
-tlFirst.from(".healthcare__logo", {y:0, opacity:0, duration:1.5})
-tlFirst.from(".healthcare__info", {y:0, opacity:0, duration:1.5},"-=1")
+tlFirst.from(".healthcare__logo", { y: 0, opacity: 0, duration: 1.5 });
+tlFirst.from(".healthcare__info", { y: 0, opacity: 0, duration: 1.5 }, "-=1");
 
 // Second animation
 let tlSecond = gsap.timeline({
-  scrollTrigger:{
-    trigger:".media",
-    start:"center bottom "
-  }
-})
+  scrollTrigger: {
+    trigger: ".media",
+    start: "center bottom ",
+  },
+});
 
-tlFirst.from(".media__logo", {y:0, opacity:0, duration:1.5})
-tlFirst.from(".media__info", {y:0, opacity:0, duration:1.5},"-=1")
+tlFirst.from(".media__logo", { y: 0, opacity: 0, duration: 1.5 });
+tlFirst.from(".media__info", { y: 0, opacity: 0, duration: 1.5 }, "-=1");
 
 // Third animation
 
-
 let tlThird = gsap.timeline({
-  scrollTrigger:{
-    trigger:".testimonial",
-    start:"center bottom ",
-  }
-})
+  scrollTrigger: {
+    trigger: ".testimonial",
+    start: "center bottom ",
+  },
+});
 
-tlThird.fromTo(".testimonial__describtion", {x:-800,},{x:0, duration:1.5,delay:.5})
-tlThird.fromTo(".testimonial__photo", {x:-800},{x:0, duration:1.5},"-=.5")
-tlThird.fromTo(".testimonial__quote", {x:800},{x:0, duration:1.5},"-=.5")
-
+tlThird.fromTo(
+  ".testimonial__describtion",
+  { x: -800 },
+  { x: 0, duration: 1.5, delay: 0.5 }
+);
+tlThird.fromTo(
+  ".testimonial__photo",
+  { x: -800 },
+  { x: 0, duration: 1.5 },
+  "-=.5"
+);
+tlThird.fromTo(
+  ".testimonial__quote",
+  { x: 800 },
+  { x: 0, duration: 1.5 },
+  "-=.5"
+);
 
 // Forth animation
 let tlForth = gsap.timeline({
-  scrollTrigger:{
-    trigger:".treatment",
-    start:"center bottom "
-  }
-})
-
-tlForth.from(".box--1", {x:-1000, opacity:0, duration:1.5})
-tlForth.from(".box--2", {x:-1000, opacity:0, duration:1.5},"-=1")
-tlForth.from(".box--3", {x:-1000,  opacity:0, duration:1.5},"-=1.5")
-
-
-gsap.fromTo(".main__left", {opacity: 0,x:-1000}, {opacity: 1, duration: 1,x:0});
-gsap.fromTo(".main__right", {opacity: 0,x:2000}, {opacity: 1, duration: 1,x:0});
-
-
-
-gsap.from('.services__box',
-{
-  scrollTrigger:{
-    trigger:".services__box",
-    start:"center bottom"
+  scrollTrigger: {
+    trigger: ".treatment",
+    start: "center bottom ",
   },
-  x:-1000,
-  duration:1.5,
-})
+});
 
+tlForth.from(".box--1", { x: -1000, opacity: 0, duration: 1.5 });
+tlForth.from(".box--2", { x: -1000, opacity: 0, duration: 1.5 }, "-=1");
+tlForth.from(".box--3", { x: -1000, opacity: 0, duration: 1.5 }, "-=1.5");
 
+gsap.fromTo(
+  ".main__left",
+  { opacity: 0, x: -1000 },
+  { opacity: 1, duration: 1, x: 0 }
+);
+gsap.fromTo(
+  ".main__right",
+  { opacity: 0, x: 2000 },
+  { opacity: 1, duration: 1, x: 0 }
+);
 
-//Cursor animation
-const mouseCursor = document.querySelector('#cursor');
-const links = document.querySelectorAll('a');
-console.log(links);
-const cursor = (e)=>{
-  mouseCursor.style.top = e.pageY + 'px';
-  mouseCursor.style.left= e.pageX + 'px';
+gsap.from(".services__box", {
+  scrollTrigger: {
+    trigger: ".services__box",
+    start: "center bottom",
+  },
+  x: -1000,
+  duration: 1.5,
+});
 
-}
+const windowScroll = () => {
+  const nav = document.querySelector(".navigation");
 
-links.forEach(link=>{
-  link.addEventListener('mouseover',()=>{
-    mouseCursor.classList.add('grow')
-  })
-
-  link.addEventListener('mouseleave',()=>{
-    mouseCursor.classList.remove('grow')
-  })
-})
-
-
-
-window.addEventListener('mousemove',cursor);
-
-
-
-
-const windowScroll = ()=>{
-
-
-  const nav = document.querySelector('.header__nav');
-  
-  
-  if(window.pageYOffset >= 100){
-    nav.classList.add('nav-sticky')
-  }else{
-    nav.classList.remove('nav-sticky')
+  if (window.pageYOffset >= 100) {
+    nav.classList.add("navigation-color");
+  } else {
+    nav.classList.remove("navigation-color");
   }
-  }
+};
 
+window.addEventListener("scroll", windowScroll);
 
-window.addEventListener('scroll',windowScroll);
+const headerHref = document.querySelectorAll(".nav-links li");
 
-
-const headerHref = document.querySelectorAll('.header__href')
-
-function selectedLink(){
-  removeActive()
-  this.classList.add('active')
+function selectedLink() {
+  removeActive();
+  this.classList.add("active-link");
 }
 
-function removeActive(){
-  headerHref.forEach(href=>href.classList.remove('active'))
+function removeActive() {
+  headerHref.forEach((href) => href.classList.remove("active-link"));
 }
-  headerHref.forEach(href=>{
-    href.addEventListener('click',selectedLink)
-    
-  })
+headerHref.forEach((href) => {
+  href.addEventListener("click", selectedLink);
+});
+
+const navSlide = () => {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav-links");
+
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("navik-active");
+  });
+  burger.addEventListener("click", () => {
+    burger.classList.toggle("toggle");
+  });
+};
+
+navSlide();
